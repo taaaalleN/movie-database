ccc03f30dc96d6908586ae415bfcd2c801c768fd
taaaalleN <patricklindbom@hotmail.com>
2020-06-10 17:22:14 +0200

    Added movieReducer, added watchlist functions test

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -2,15 +2,36 @@ export const movieReducer = (state, action) => {
   switch (action.type) {
     case "FETCH_SUCCESS":
       return {
-        data: [...action.payload, { watchlisted: false }],
+        ...state,
+        data: [...action.payload],
         loading: false,
         error: "",
       };
     case "FETCH_ERROR":
       return {
+        ...state,
         data: [],
         loading: false,
         error: action.payload,
+      };
+    case "RESET_MOVIES":
+      return {
+        ...state,
+        data: [],
+        loading: false,
+        error: "",
+      };
+    case "ADD_TO_WATCHLIST":
+      return {
+        ...state,
+        watchlist: [...state.watchlist, action.payload],
+      };
+    case "REMOVE_FROM_WATCHLIST":
+      console.log(action.payload);
+      const newWatchlist = state.watchlist.filter((movie) => movie.id !== action.payload.id);
+      return {
+        ...state,
+        watchlist: newWatchlist,
       };
     default:
       return state;
