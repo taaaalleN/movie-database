c0b0e43ce489421bf95360cee5935100240c2755
taaaalleN <patricklindbom@hotmail.com>
2020-06-10 17:12:09 +0200

    Added constant

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+export const API_KEY = "04885294e995c2b055be7cf3da2429ed";
+export const BASEURL = `https://api.themoviedb.org/3`;
+export const DISCOVER = "/discover";
+export const POPULAR = "/popular";
+export const TOP_RATED = "/top_rated";
+export const UPCOMING = "/upcoming";
+
+export const DEFAULT_PAGE = 1;
+export const PAGE_NUMBER = "page=";
