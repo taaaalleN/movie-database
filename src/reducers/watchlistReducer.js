1dcfeb29f28c5a52902f8df82d4f2eaabb885e08
taaaalleN <patricklindbom@hotmail.com>
2020-06-10 17:22:29 +0200

    Added watchlistReducer for future use

--- src/reducers/watchlistReducer.js
+++ src/reducers/watchlistReducer.js
@@ -1,7 +1,7 @@
 export const watchlistReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_WATCHLIST":
-      return [...state, { id: action.movie.id, title: action.movie.title, watchlisted: true }];
+      return [...state, { ...action.movie, watchlisted: true }];
     case "REMOVE_FROM_WATCHLIST":
       return state.filter((movie) => movie.id === action.id);
     // case "REMOVE_FROM_WATCHLIST":
